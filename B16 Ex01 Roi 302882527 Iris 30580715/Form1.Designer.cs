07bb27cf0c6c394a6a0074d219407f618cd78a70
roiDaniela <roi.fogler@gmail.com>
2016-03-20 21:59:18 +0200

    1.2 20.3.16

--- B16 Ex01 Roi 302882527 Iris 30580715/Form1.Designer.cs
+++ B16 Ex01 Roi 302882527 Iris 30580715/Form1.Designer.cs
@@ -28,35 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LoginButton = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.picture_smallPictureBox = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_smallPictureBox)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // LoginButton
+            // buttonLogin
             // 
-            this.LoginButton.Location = new System.Drawing.Point(12, 12);
-            this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(75, 23);
-            this.LoginButton.TabIndex = 0;
-            this.LoginButton.Text = "Login";
-            this.LoginButton.UseVisualStyleBackColor = true;
-            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            this.buttonLogin.Location = new System.Drawing.Point(6, 6);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxStatus.Location = new System.Drawing.Point(139, 9);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(485, 20);
+            this.textBoxStatus.TabIndex = 46;
+            // 
+            // picture_smallPictureBox
+            // 
+            this.picture_smallPictureBox.Location = new System.Drawing.Point(6, 56);
+            this.picture_smallPictureBox.Name = "picture_smallPictureBox";
+            this.picture_smallPictureBox.Size = new System.Drawing.Size(200, 156);
+            this.picture_smallPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_smallPictureBox.TabIndex = 47;
+            this.picture_smallPictureBox.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(745, 327);
+            this.tabControl1.TabIndex = 48;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonLogin);
+            this.tabPage1.Controls.Add(this.picture_smallPictureBox);
+            this.tabPage1.Controls.Add(this.textBoxStatus);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(737, 301);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.gmap);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(737, 301);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(3, 3);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 2;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(731, 295);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.Load += new System.EventHandler(this.gmap_Load);
             // 
             // Facebook
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(745, 327);
-            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Facebook";
             this.Text = "FacebookDesktopApp";
+            ((System.ComponentModel.ISupportInitialize)(this.picture_smallPictureBox)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.PictureBox picture_smallPictureBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private GMap.NET.WindowsForms.GMapControl gmap;
     }
 }
 
--- B16 Ex01 Roi 302882527 Iris 30580715/Form1.cs
+++ B16 Ex01 Roi 302882527 Iris 30580715/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET;
 
 namespace B16_Ex01_Roi_302882527_Iris_30580715
 {
@@ -15,13 +20,128 @@ namespace B16_Ex01_Roi_302882527_Iris_30580715
         public Facebook()
         {
             InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 1000;
         }
 
-        private void LoginButton_Click(object sender, EventArgs e)
+        User m_LoggedInUser;
+        List<Checkin> m_LocationCheckInList = new List<Checkin>();
+
+        private void loginAndInit()
         {
-            Console.WriteLine();
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("1587985424858781", 
+                "public_profile",
+                "user_education_history",
+                "user_birthday",
+                "user_actions.video",
+                "user_actions.news",
+                "user_actions.music",
+                "user_actions.fitness",
+                "user_actions.books",
+                "user_about_me",
+                "user_friends",
+                "publish_actions",
+                "user_events",
+                "user_games_activity",
+                //"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_hometown",
+                "user_likes",
+                "user_location",
+                "user_managed_groups",
+                "user_photos",
+                "user_posts",
+                "user_relationships",
+                "user_relationship_details",
+                "user_religion_politics",
+
+                //"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_tagged_places",
+                "user_videos",
+                "user_website",
+                "user_work_history",
+                "read_custom_friendlists",
+
+                // "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "read_page_mailboxes",
+                // "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                // "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "manage_pages",
+                "publish_pages",
+                "publish_actions",
+
+                "rsvp_event"
+                );
+            // These are NOT the complete list of permissions. Other permissions for example:
+            // "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
+            // The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                fetchUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
         }
 
+        private void fetchUserInfo()
+        {
+            picture_smallPictureBox.LoadAsync(m_LoggedInUser.PictureNormalURL);
+            if (m_LoggedInUser.Posts.Count > 0)
+            {
+                textBoxStatus.Text = m_LoggedInUser.Posts[0].Message;
+            }
+            
+            // Add all chekins
+            foreach (Checkin checkin in m_LoggedInUser.Checkins)
+            {
+                m_LocationCheckInList.Add(checkin);
+            }
+        }
 
+        private void showMarkersOnMap()
+        {
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            double dLongitude;
+            double dLatitude;
+
+            foreach (Checkin item in m_LocationCheckInList)
+            {
+                dLongitude = Convert.ToDouble(item.Place.Location.Longitude);
+                dLatitude = Convert.ToDouble(item.Place.Location.Latitude);
+
+                GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(dLongitude, dLatitude), GMarkerGoogleType.red);
+                marker.ToolTipText = item.Place.Location.City + ", " + item.Place.Location.State + " " + "(" + item.CreatedTime.ToString() + ")";
+                markersOverlay.Markers.Add(marker);
+                gmap.Overlays.Add(markersOverlay);
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
+        private void buttonSetStatus_Click(object sender, EventArgs e)
+        {
+            Status postedStatus = m_LoggedInUser.PostStatus(textBoxStatus.Text);
+            MessageBox.Show("Status Posted! ID: " + postedStatus.Id);
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            // Initialize map:
+            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.DragButton = System.Windows.Forms.MouseButtons.Left;
+            gmap.Zoom = 1;
+
+            showMarkersOnMap();
+        }
     }
 }
